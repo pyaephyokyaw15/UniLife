610e632c25053cda1ae79ca8aab82778587f737f
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-04-24 23:13:21 +0630

    naming convention

--- api/serializers.py
+++ api/serializers.py
@@ -11,11 +11,11 @@ class AuthorInfoSerializer(serializers.Serializer):
 
 class PostSerializer(serializers.ModelSerializer):
     posted_by = AuthorInfoSerializer(source='author', read_only=True)
-    banner = Base64ImageField(required=False)
+    image = Base64ImageField(required=False)
 
     class Meta:
         model = Post
-        fields = ['posted_by', 'title', 'content', 'date_posted', 'banner']
+        fields = ['posted_by', 'title', 'content', 'created_date', 'image']
 
     def create(self, validated_data):  # override the create method
         request = self.context.get('request')
--- post/models.py
+++ post/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    banner = models.ImageField(default='default.jpg')   # , upload_to='banner'
+    image = models.ImageField(default='default.jpg')   # , upload_to='banner'
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now, editable=False)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
 
 
